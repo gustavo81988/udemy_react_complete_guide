87d208dc255bef674b4c956539e6f18514c660c3
Gustavo Ramirez <gramirez@overall.com.pe>
2022-07-16 17:46:50 -0500

    91. Adding a re-usable Card Component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Card from '../UI/Card';
+import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
     const AddUserHandler = (event) =>{
@@ -6,15 +8,17 @@ const AddUser = (props) => {
     }
 
     return(
-        <form onSubmit={AddUserHandler}>
-            <label htmlFor="username">Username</label>
-            <input id="username" type="text"/>
+        <Card className={classes.input}>
+            <form onSubmit={AddUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input id="username" type="text"/>
 
-            <label htmlFor="age">Age (Years)</label>
-            <input id="age" type="number"/>
-            
-            <button type="submit">Add User</button>
-        </form>
+                <label htmlFor="age">Age (Years)</label>
+                <input id="age" type="number"/>
+                
+                <button type="submit">Add User</button>
+            </form>
+        </Card>
     );
 };
 
